eed1045870b8448ff7844554c5e1ea8a1c76f977
skinnynoonie <101953182+skinnynoonie@users.noreply.github.com>
2023-10-08 18:28:54 -0400

    Init the server api

---
+++ CharacterWar/src/main/java/me/skinnynoonie/characterwar/CharacterWar.java
@@ -0,0 +1,16 @@
+package me.skinnynoonie.characterwar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CharacterWar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
